ea9f8a16662dd4e2448a64d1ffa28c143af4f6e7
HyeJune Suh <hyejune.suh@gmail.com>
2017-11-16 03:15:18 +0900

    maps backup

--- src/pages/navigation/navigation.ts
+++ src/pages/navigation/navigation.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 declare var google;
+
+declare var google: any;
 
 @Component({
   selector: 'page-nav',
@@ -10,9 +12,47 @@ export class NavPage {
   Destination: any = '';
   MyLocation: any;
 
+  @ViewChild('map') mapRef: ElementRef;
+  // map: any;
+
+
   constructor(public navCtrl: NavController) {
 
   }
+  ionViewDidLoad() {
+    this.showMap();
+    console.log(this.mapRef);
+  }
+
+  showMap() {
+    //location - lat long
+    const location = new google.maps.LatLng(51.507351, -0.127758)
+
+    //map oprions
+    const options = {
+      center: location,
+      zoon: 15,
+      streetViewControl: false,
+      mapTypeId: 'roadmap'
+  }
+
+  const map = new google.maps.Map(this.mapRef.nativeElement, options);
+  console.log("SHOWED");
+
+
+
+  this.addMarker(location, map);
+  }
+
+  addMarker(position, map) {
+    return new google.maps.Marker({
+      position,
+      map
+    });
+
+  }
+
+
 
   calculateAndDisplayRoute() {
         let that = this;
