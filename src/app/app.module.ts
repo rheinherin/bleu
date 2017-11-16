3b2884ad8f0a5a77fff7097f1b94a26b9976e149
HyeJune Suh <hyejune.suh@gmail.com>
2017-11-16 11:48:45 +0900

    added bt codes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
+
 
 @NgModule({
   declarations: [
@@ -37,6 +39,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    BluetoothSerial,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
