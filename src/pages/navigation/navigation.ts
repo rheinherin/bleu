465e126c5abcf1d107ba37a8b0be570fb9453f0f
HyeJune Suh <hyejune.suh@gmail.com>
2017-11-16 17:12:26 +0900

    logically perfect

--- src/pages/navigation/navigation.ts
+++ src/pages/navigation/navigation.ts
@@ -1,49 +1,85 @@
+
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 
 declare var google;
 
-// declare var google: any;
 
+// declare var google: any;
 @Component({
   selector: 'page-nav',
   templateUrl: 'navigation.html'
 })
 export class NavPage {
-  // Destination: any = '';
-  // MyLocation: any;
-
+  Destination: any = '';
+  MyLocation: any;
   @ViewChild('map') mapElement: ElementRef;
   @ViewChild('directionsPanel') directionsPanel: ElementRef;
   map: any;
   // map: any;
 
-
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public geolocation: Geolocation) {
 
   }
   ionViewDidLoad() {
     this.loadMap();
-    this.startNavigating();
+    // this.startNavigating();
 
     }
 
     loadMap(){
+      this.geolocation.getCurrentPosition().then((position) => {
 
-        let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-        let mapOptions = {
-          center: latLng,
-          zoom: 15,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
 
-        this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    }, (err) => {
+      console.log(err);
+    });
+
 
     }
+    addMarker(){
+
+        let marker = new google.maps.Marker({
+          map: this.map,
+          animation: google.maps.Animation.DROP,
+          position: this.map.getCenter()
+        });
+
+        let content = "<h4>Information!</h4>";
+
+        this.addInfoWindow(marker, content);
+
+      }
+    addInfoWindow(marker, content){
+
+        let infoWindow = new google.maps.InfoWindow({
+          content: content
+        });
+
+        google.maps.event.addListener(marker, 'click', () => {
+          infoWindow.open(this.map, marker);
+        });
+
+      }
 
     startNavigating(){
+        // let that = this;
+        this.geolocation.getCurrentPosition().then((position) => {
+        let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        console.log(position.coords.latitude);
+        console.log(position.coords.longitude);
+        console.log(position.coords.latitude+", "+position.coords.longitude);
+        console.log(this.Destination);
 
         let directionsService = new google.maps.DirectionsService;
         let directionsDisplay = new google.maps.DirectionsRenderer;
@@ -52,8 +88,8 @@ export class NavPage {
         directionsDisplay.setPanel(this.directionsPanel.nativeElement);
 
         directionsService.route({
-            origin: 'adelaide',
-            destination: 'adelaide oval',
+            origin: toString(position.coords.latitude)+", "+toString(position.coords.longitude),
+            destination: toString(this.Destination),
             travelMode: google.maps.TravelMode['DRIVING']
         }, (res, status) => {
 
@@ -65,6 +101,10 @@ export class NavPage {
 
         });
 
-    }
+
+  }, (err) => {
+    console.log(err);
+  });
+}
 
 }
